e4b94f659ef3e5d8276c42023b60a3c38939e9e5
Giovanni Serafim <giovannirafael0123@gmail.com>
2024-11-04 20:44:17 -0300

    finalizando o exercício 2

--- Estudos JavaScript/exercícios/ex-03/array.js
+++ Estudos JavaScript/exercícios/ex-03/array.js
@@ -5,9 +5,27 @@ var array = []
 function inserir(){
 
 
+
+
 //pega o numero
 var num = document.getElementsByName("num")[0].value
 
+//se o numero ja estiver na lista
+if(array.includes(num)){
+    window.alert("numero ja inserido")
+    document.getElementsByName("num")[0].value = "" //limpa o campo
+
+}
+
+//se o numero exeder o limite
+else if (num < 0 || num > 100){
+    window.alert("o numero deve estar entre 0 e 100")
+    document.getElementsByName("num")[0].value = ""
+
+}
+
+
+else{
 //cria o option 
 var option = document.createElement("option")
 option.text = `o ${num} foi inserido`
@@ -19,8 +37,30 @@ lista.add(option)
 array.push(num)
 
 document.getElementsByName("num")[0].value = ""
+}
 }// fim do inserir()
 
 function resultado(){
- document.getElementById("resultado").innerHTML = "[" + array + "]"
+
+ if (array.length == 0){
+    window.alert("adicione intens antes de finalizar")
+    document.getElementsByName("num")[0].value = ""
+      }
+
+else{
+
+var maior = Math.max(...array)
+
+var menor = Math.min(...array)
+
+var quantidade = array.length
+
+var soma = array.reduce((x, num)=> x + Number(num), 0 )
+
+var media = soma / quantidade
+
+
+ document.getElementById("resultado").innerHTML = `quantidade: ${quantidade} <br> maior: ${maior} <br> menor: ${menor} <br> soma: ${soma} <br> média: ${media}`
+}
+ 
 }
