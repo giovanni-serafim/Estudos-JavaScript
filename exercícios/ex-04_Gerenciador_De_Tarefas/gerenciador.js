cd3a1eb11fd49f0fbbb3e1c558779b9543c0164b
Giovanni Serafim <giovannirafael0123@gmail.com>
2024-12-03 21:29:47 -0300

    consertando design

--- exercícios/ex-04_Gerenciador_De_Tarefas/gerenciador.js
+++ exercícios/ex-04_Gerenciador_De_Tarefas/gerenciador.js
@@ -1,10 +1,16 @@
-function adicionar(){
+function adicionar() {
+    // Obter a referência do contêiner onde as tarefas estão (section)
+    const sectionTarefas = document.getElementById("tarefas");
+    
+    // Clonar o último elemento .tarefa
+    const novaTarefa = sectionTarefas.lastElementChild.cloneNode(true);
+    
+    // Limpar os campos do formulário da nova tarefa
+    const inputs = novaTarefa.getElementsByTagName("input");
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].value = ""; // Limpar os valores
+    }
 
-    var div = document.getElementsByTagName("div")[0]
-
-    var novaDiv = div.cloneNode(true)
-
-    document.querySelector("section").appendChild(novaDiv);
-
-
+    // Adicionar a nova tarefa à seção
+    sectionTarefas.appendChild(novaTarefa);
 }
